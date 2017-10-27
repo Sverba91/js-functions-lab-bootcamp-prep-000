8fa3b8bc74b5569c058cb98ab069db0b7320e23e
Sofya Verba <sofyaverba@gmail.com>
2017-10-27 01:55:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function happyHolidays (string) {
   return "Happy holidays!"
 }
-function happyHolidaysto (name) {
-  return `Happy holidays, ${name}!`
+function happyHolidaysto (string) {
+  return `Happy holidays, ${kevin}!`
 }
